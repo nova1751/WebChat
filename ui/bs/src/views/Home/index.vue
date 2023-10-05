df26a875db7195d87d3985f507b1aa4c4937b504
nova1751 <nova2319427728@outlook.com>
2023-10-05 22:22:34 +0800

    chore: cs ui project init

---
+++ ui/bs/src/views/Home/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const count = ref(0);
+const handleClick = () => {
+  count.value++;
+  ElMessage({
+    type: 'success',
+    message: `${count.value}`,
+  });
+};
+</script>
+<template>
+  <div>
+    <el-button @click="handleClick" type="primary">test</el-button>
+    {{ count }}
+  </div>
+</template>
+<style scoped lang="scss"></style>
