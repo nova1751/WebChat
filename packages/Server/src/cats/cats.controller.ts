0ba4697a01373fdaed5d5056a63d9d1a9956401b
nova1751 <nova2319427728@outlook.com>
2023-12-20 13:42:53 +0800

    docs: update

--- packages/Server/src/cats/cats.controller.ts
+++ packages/Server/src/cats/cats.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param } from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
+  }
+}
